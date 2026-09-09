77cd31ed905d43b8a44f30bb15569a737023a09c
gswathvika23-hub <gswathvika23@gmail.com>
2026-09-09 16:12:30 +0530

    Point frontend to live Render backend URL

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+   baseURL: "https://smart-expense-budget-tracker-temy.onrender.com",
 });
 
 // Automatically attach the JWT token to every request, if one exists
